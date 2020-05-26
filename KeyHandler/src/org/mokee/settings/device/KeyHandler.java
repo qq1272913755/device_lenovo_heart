87d837beeb01d155d6beec38e5019dd03b99369f
ZhouYu2003 <62654837+ZhouYu2003@users.noreply.github.com>
2020-05-26 23:04:25 +0800

    Update KeyHandler.java

--- KeyHandler/src/org/mokee/settings/device/KeyHandler.java
+++ KeyHandler/src/org/mokee/settings/device/KeyHandler.java
@@ -17,43 +17,66 @@
 package org.mokee.settings.device;
 
 import android.content.Context;
-import android.media.AudioManager;
-import android.os.Vibrator;
-import android.view.KeyEvent;
 
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.SystemClock;
+import android.os.Vibrator;
+import android.telecom.TelecomManager;
+import android.util.Log;
+import android.view.KeyEvent;
 import com.android.internal.os.DeviceKeyHandler;
 
 public class KeyHandler implements DeviceKeyHandler {
     private static final String TAG = KeyHandler.class.getSimpleName();
 
     // Slider key codes
-    private static final int MODE_NORMAL = 601;
-    private static final int MODE_VIBRATION = 602;
-    private static final int MODE_SILENCE = 603;
+    private static final int SLIDER_CLOSED = 221;
+    private static final int SLIDER_OPENED = 220;
+
+    private static final int SWITCH_WAKELOCK_DURATION = 3000;
 
     private final Context mContext;
-    private final AudioManager mAudioManager;
+
     private final Vibrator mVibrator;
+    private final TelecomManager mTelecomManager;
+    private final PowerManager mPowerManager;
+    private final WakeLock mSwitchWakeLock;
 
     public KeyHandler(Context context) {
         mContext = context;
 
-        mAudioManager = mContext.getSystemService(AudioManager.class);
         mVibrator = mContext.getSystemService(Vibrator.class);
+
+        mTelecomManager = (TelecomManager) mContext.getSystemService(Context.TELECOM_SERVICE);
+        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mSwitchWakeLock = mPowerManager.newWakeLock(
+                PowerManager.PARTIAL_WAKE_LOCK, "SwitchWakeLock");
     }
+
 
     public KeyEvent handleKeyEvent(KeyEvent event) {
         int scanCode = event.getScanCode();
 
         switch (scanCode) {
-            case MODE_NORMAL:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+            case SLIDER_CLOSED:
+                Log.e(TAG, "SLIDER_CLOSED");
+                if (mTelecomManager != null) {
+                        if (mTelecomManager.isInCall()) {
+                                mTelecomManager.endCall();
+                        };
+                };
                 break;
-            case MODE_VIBRATION:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
-                break;
-            case MODE_SILENCE:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+            case SLIDER_OPENED:
+                Log.e(TAG, "SLIDER_OPENED");
+                if (mTelecomManager != null) {
+                        if (mTelecomManager.isRinging()) {
+                                mTelecomManager.acceptRingingCall();
+                         };
+                };
+                //wakeup display on slider opened
+                mSwitchWakeLock.acquire(SWITCH_WAKELOCK_DURATION);
+                mPowerManager.wakeUp(SystemClock.uptimeMillis(), "switch-wakeup");
                 break;
             default:
                 return event;
